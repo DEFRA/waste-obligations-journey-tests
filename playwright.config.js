4e2f1d75adb40fe730ab4d44525d2310924c332a
Francis Chelladurai <francis.chelladurai@equalexperts.com>
2026-06-04 14:30:15 +0100

    MO-233 updated proxy config

--- playwright.config.js
+++ playwright.config.js
@@ -40,7 +40,7 @@ export default defineConfig({
   use: {
     baseURL,
     ignoreHTTPSErrors: true,
-    trace: 'on-first-retry',
+    trace: 'on',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     actionTimeout: 15_000,
